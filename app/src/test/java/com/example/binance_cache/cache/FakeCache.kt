a3ba5c24d8023e0a75e74e4298891b7aada35fbe
ayhamorfali <orfali.ayham@gmail.com>
2023-05-08 16:12:27 +0400

    Test - Added Cache unit tests

---
+++ app/src/test/java/com/example/binance_cache/cache/FakeCache.kt
@@ -0,0 +1,15 @@
+package com.example.binance_cache.cache
+
+class FakeCache<K, V> : Cache<K, V> {
+
+    private val store = HashMap<K, V>()
+
+    override fun put(key: K, value: V) = store.put(key, value)
+
+    override fun fetch(key: K) = store[key]
+
+    override fun delete(key: K) = store.remove(key)
+
+    override fun count(value: V) = store.count { it.value == value }
+
+}
---
+++ app/src/test/java/com/example/binance_cache/cache/SimpleCacheTest.kt
@@ -0,0 +1,52 @@
+package com.example.binance_cache.cache
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class SimpleCacheTest {
+    private lateinit var cache: SimpleCache<String, String>
+
+    @Before
+    fun setUp() {
+        cache = SimpleCache()
+    }
+
+    @Test
+    fun `should store key value pairs and fetch a value by key`() {
+        val key = "foo"
+        val value = "123"
+
+        cache.put(key, value)
+
+        assertEquals(cache.fetch(key), value)
+    }
+
+    @Test
+    fun `should delete a pair given the key`() {
+        val key = "foo"
+        val value = "123"
+
+        cache.put(key, value)
+        cache.delete(key)
+
+        assertNull(cache.fetch(key))
+    }
+
+    @Test
+    fun `should count value occurrences`() {
+        val key1 = "foo"
+        val value1 = "123"
+        val key2 = "bar"
+        val key3 = "baz"
+        val value3 = "abc"
+
+        cache.put(key1, value1)
+        cache.put(key2, value1)
+        cache.put(key3, value3)
+
+        assertEquals(cache.count(value1), 2)
+        assertEquals(cache.count(value3), 1)
+        assertEquals(cache.count(key1), 0)
+    }
+}
